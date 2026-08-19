9acb9b7aacd947d4e8fa620aa4d537617f7f5ac0
Ziad003 <ziad.hossain15@gmail.com>
2026-08-19 23:53:33 +0600

    last

--- src/practice2.ts
+++ src/practice2.ts
@@ -1,10 +1,8 @@
-class Person{
-  name:string;
-  constructor(name:string){
-    this.name=name
+type Generic<T>=Array<T>
+
+const arr:Generic<{name:string; age:number}>=[
+  {
+    name:'a',
+    age:23
   }
-}
-
-const showPerson=(user:Person)=>{
-
-}
+]
