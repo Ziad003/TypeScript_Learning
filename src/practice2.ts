f7ad9bd4b0f104107e869875f53d03d248c9cc13
Ziad003 <ziad.hossain15@gmail.com>
2026-08-22 00:47:43 +0600

    change in generic

--- src/practice2.ts
+++ src/practice2.ts
@@ -1,8 +1,25 @@
-type Generic<T>=Array<T>
+type GenericThings<T>=T
+type GenericArray<T>=Array<T>
+type GenericObj<X>={
+  name:string,
+  age:number,
+  bike:X
+}
 
-const arr:Generic<{name:string; age:number}>=[
-  {
-    name:'a',
-    age:23
-  }
-]
+
+
+const name:GenericThings<string>="Ziad"
+
+const array:GenericArray<string>=['A',"b"]
+
+const obj:GenericObj<number>={
+  name:'Ziad',
+  age:25,
+  bike:4
+}
+
+const addFun=<T,X>(num1:T,num2:X):T=>{
+  return num1 
+}
+
+addFun(3,'d')
